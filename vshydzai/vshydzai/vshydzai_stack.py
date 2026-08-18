7285716eec7d964c8e80521a749184ed563eaf2b
Van Chan Vi Dong <vi16082006@gmail.com>
2026-08-18 11:07:06 +1000

    Add Hello World Lambda function

--- vshydzai/vshydzai/vshydzai_stack.py
+++ vshydzai/vshydzai/vshydzai_stack.py
@@ -1,19 +1,22 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    RemovalPolicy,
+    aws_lambda as _lambda,
 )
 from constructs import Construct
+
 
 class VshydzaiStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        hello_function = _lambda.Function(
+            self,
+            "HelloWorldFunction",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="hello.lambda_handler",
+            code=_lambda.Code.from_asset("lambda_src"),
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "VshydzaiQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        hello_function.apply_removal_policy(RemovalPolicy.DESTROY)
